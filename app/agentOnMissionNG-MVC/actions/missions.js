deaa97312700c6b0331e1acf7c7bd980d40b04ce
lironh <lironenk@gmail.com>
2017-05-13 20:43:17 +0300

    feat(added ngredux)

--- app/agentOnMissionNG-MVC/actions/missions.js
+++ app/agentOnMissionNG-MVC/actions/missions.js
@@ -4,25 +4,23 @@ angular.module('agentMission')
     .factory('missionsAction', function (_, $q, mainService) {
 
         function findIsolatedCountry (input){
-            return function (dispatch) {
-                   var mapCountryToAgent = {}; // {agent : [countryA, countryB]}
-                   var mapIsoAgentToCountry = {}; // { country: [iso-agentA, iso-agentB]}
-
-                    input.forEach(function (item) {
+            return dispatch => {
+                   const mapCountryToAgent = {}; // {agent : [countryA, countryB]}
+                   const mapIsoAgentToCountry = {}; // { country: [iso-agentA, iso-agentB]}
+                    input.forEach( item => {
                         // map the agents to the countries they were at
                         if (!mapCountryToAgent[item.agent]) {
                             mapCountryToAgent[item.agent] = [];
                         }
                         mapCountryToAgent[item.agent].push(item.country);
                     });
-
                  //  keep only isolated agents - agents who has just one country
-                    for (var agent in mapCountryToAgent) {
+                    for (let agent in mapCountryToAgent) {
                         if(mapCountryToAgent[agent].length > 1){
                             delete mapCountryToAgent[agent]; // remove agents which aren't isolated
                         }
                         else {
-                            var country = mapCountryToAgent[agent][0];
+                            const country = mapCountryToAgent[agent][0];
                             if (!mapIsoAgentToCountry[country]) { // start fill in the country : agents[] map
                                 mapIsoAgentToCountry[country] = [];
                             }
@@ -30,7 +28,7 @@ angular.module('agentMission')
                         }
                     }
                  //  get the country who has the most isolated agents
-                    var isoCountry = _.max(Object.keys(mapIsoAgentToCountry), function (prop) {
+                    var isoCountry = _.max(Object.keys(mapIsoAgentToCountry), prop => {
                         return mapIsoAgentToCountry[prop].length;
                     });
 
