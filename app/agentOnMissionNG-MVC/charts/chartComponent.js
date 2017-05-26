edeb3b2cfa2de102a8bd8785c9e1927bec41715a
lironh <lironenk@gmail.com>
2017-05-26 22:10:47 +0300

    feat(added ngredux)

--- app/agentOnMissionNG-MVC/charts/chartComponent.js
+++ app/agentOnMissionNG-MVC/charts/chartComponent.js
@@ -7,177 +7,75 @@
 
 function nvd3ChartController ($scope) {
 
-//    $scope.options = {
-//        chart: {
-//            type: 'scatterChart',
-//            height: 450,
-//            margin : {
-//                top: 20,
-//                right: 20,
-//                bottom: 40,
-//                left: 55
-//            },
-//            x: function(d){ return d.x; },
-//            //y: function(d){ return d.y; },
-//            useInteractiveGuideline: true,
-//            dispatch: {
-//                stateChange: function(e){ console.log("stateChange"); },
-//                changeState: function(e){ console.log("changeState"); },
-//                tooltipShow: function(e){ console.log("tooltipShow"); },
-//                tooltipHide: function(e){ console.log("tooltipHide"); }
-//            },
-//            xAxis: {
-//                axisLabel: 'Time (ms)',
-//                  tickFormat: function(d){
-//                     return d3.format('.02f')(d);
-//                     }
-//            },
-//            yAxis: {
-//                axisLabel: 'Voltage (v)',
-//                tickFormat: function(d){
-//                    return d3.format('.02f')(d);
-//                },
-//                axisLabelDistance: -10
-//            },
-//            callback: function(chart){
-//                console.log("!!! lineChart callback !!!");
-//            }
-//        },
-//        title: {
-//            enable: true,
-//            text: 'Title for Line Chart'
-//        },
-//        subtitle: {
-//            enable: true,
-//            text: 'Subtitle for simple line chart..',
-//            css: {
-//                'text-align': 'center',
-//                'margin': '10px 13px 0px 7px'
-//            }
-//        },
-///*        caption: {
-//            enable: true,
-//            html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-//            css: {
-//                'text-align': 'justify',
-//                'margin': '10px 13px 0px 7px'
-//            }
-//        }*/
-//    };
-
 $scope.options = {
     chart: {
       type: 'scatterChart',
-      height: 450,
-     // color: d3.scale.category10().range(),
-//      scatter: {
-//        onlyCircles: false
-//      },
-      //showDistX: true,
-      //showDistY: true,
+      height: 350,
+//      tooltip: {
+//            contentGenerator: function (key, x, y, e, graph) {
+//              return key.value;
+//            }
+//          },
+    //  useInteractiveGuideline: false,
       tooltipContent: function(key) {
-        return key;
+        return 'yellow';
       },
-      pointRange: [300, 300], // size of points
+
+      //yDomain: [0,100],
+      pointRange: [150, 150], // size of points
+
       dispatch: {
-//        renderEnd: function(e) {
-//          console.log('renderEnd')
-//          $scope.showLabel();
-//        }
       },
-     // duration: 350,
+
       xAxis: {
-        axisLabel: 'X Axis',
+        axisLabel: 'Events',
+        axisLabelDistance: 5,
         tickFormat: function(d) {
-          return d3.format('.02f')(d);
+          return d3.format('.0d')(d); //d3.time.format('%Y-%m-%d')(new Date(d));
         }
       },
+
       yAxis: {
-        axisLabel: 'Y Axis',
+        axisLabel: 'Price per share',
         tickFormat: function(d) {
-          return d3.format('.02f')(d);
+          return d3.format('.0d')(d);
         },
-        axisLabelDistance: -5
       },
-            callback: function(chart){
-               $scope.showLabel();
-            },
-//      zoom: {
-//        //NOTE: All attributes below are optional
-//        enabled: false,
-//        scaleExtent: [1, 10],
-//        useFixedDomain: true,
-//        useNiceScale: true,
-//        horizontalOff: true,
-//        verticalOff: true,
-//        unzoomEventType: 'dblclick.zoom'
-//      }
+
+      callback: function(chart){
+       // $scope.showLabel();
+      },
     }
   };
+
+
   $scope.clear= function(){
     d3.selectAll(".label").remove()
   }
-  $scope.showLabel= function(){
-    $scope.clear();
-    d3.selectAll(".nv-groups path")[0].forEach(function(d){
-          var tf = d3.select(d).attr("transform")
-          var t = d3.transform(tf).translate;
-          t[0] = t[0] -5;//moving the translate x by 5 pixel.
-          //console.log(d3.select(d).data()[0])//data associated with teh point
-          d3.select(d.parentNode)
-            .append("text")
-            .attr("class", "label")
-            .html(function(d, i){
-                    return d.event;
-             })
-            .attr("transform", "translate("+t[0]+","+t[1]+")")
-            .attr('dy', 5)
-            .style("font-size", "15px")
-    });
 
-  }
-
-
-
-    /* $scope.options = {
-     chart: {
-     type: 'scatterChart',
-     height: 450,
-     color: d3.scale.category10().range(),
-     scatter: {
-     onlyCircles: false
-     },
-     showDistX: true,
-     showDistY: true,
-     //tooltipContent: function(d) {
-     //    return d.series && '<h3>' + d.series[0].key + '</h3>';
-     //},
-     duration: 350,
-     xAxis: {
-     axisLabel: 'X Axis',
-     tickFormat: function(d){
-     return d3.format('.02f')(d);
-     }
-     },
-     yAxis: {
-     axisLabel: 'Y Axis',
-     tickFormat: function(d){
-     return d3.format('.02f')(d);
-     },
-     axisLabelDistance: -5
-     },
-     zoom: {
-     //NOTE: All attributes below are optional
-     enabled: true,
-     scaleExtent: [1, 10],
-     useFixedDomain: false,
-     useNiceScale: false,
-     horizontalOff: false,
-     verticalOff: false,
-     unzoomEventType: 'dblclick.zoom'
-     }
-     }
-     };*/
+//  $scope.showLabel= function(){
+//  //  $scope.clear();
+//    var index = -1;
+//    var letters = 'ABCDEFG'.split(''); //['A', 'B', 'C'];
+//    d3.selectAll(".nv-groups path")[0].forEach(function(d){
+//          index ++;
+//          var tf = d3.select(d).attr("transform")
+//          var t = d3.transform(tf).translate;
+//          t[0] = t[0] -5;//moving the translate x by 5 pixel.
+//          //console.log(d3.select(d).data()[0])//data associated with teh point
+//          d3.select(d.parentNode)
+//            .append("text")
+//            //.attr("class", "event-text")
+//            .html(letters[index])
+//            .attr("transform", "translate("+t[0]+","+t[1]+")")
+//            .attr('dy', 5)
+//           // .attr('dx', 5)
+//            .style("font-size", "14px")
+//            .style("color", "white");
+//
+//    });
+//
+//  }
 
 }
 
@@ -186,10 +84,11 @@ angular.module('agentMission').component('nvdchart', {
         data: '<' // one way binding
     },
     controllerAs: '$ctrl',
+
     template: `
     <div class="nvd-line-chart">
         <nvd3 options="options" data="$ctrl.data" class="with-3d-shadow with-transitions"></nvd3>
     </div>
-  `,
+ `,
     controller : nvd3ChartController,
 });
--- app/agentOnMissionNG-MVC/mainCtrl.js
+++ app/agentOnMissionNG-MVC/mainCtrl.js
@@ -44,70 +44,54 @@ angular.module('agentMission')
         unsubscribe();
     });
 
-//    $scope.data = sinAndCos();
-//    /*Random Data Generator */
-//    function sinAndCos() {
-//          var sin = [],sin2 = [],
-//              cos = [];
-//
-//          //Data is represented as an array of {x,y} pairs.
-//          for (var i = 0; i < 100; i++) {
-//              sin.push({x: i, y: Math.sin(i/10)});
-//              sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-//              cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
-//          }
-//
-//          //Line chart data should be sent as an array of series objects.
-//          return [
-//              {
-//                  values: sin,      //values - represents the array of {x,y} data points
-//                  key: 'Sine Wave', //key  - the name of the series.
-//                  color: '#ff7f0e',  //color - optional: choose your own line color.
-//                  strokeWidth: 2,
-//                  classed: 'dashed'
-//              },
-///*              {
-//                  values: cos,
-//                  key: 'Cosine Wave',
-//                  color: '#2ca02c'
-//              },*/
-//              {
-//                  values: sin2,
-//                  key: 'Another sine wave',
-//                  color: '#7777ff',
-//                  area: true      //area - set to true if you want this line to turn into a filled area chart.
-//              }
-//          ];
-//      }
+      $scope.data = generateData();
 
-      $scope.data = generateData(1, 4);
+      function generateData() {
+          var letters = 'ABC'.split(''); //['A', 'B', 'C'];
+          var years = ['2013', '2014', '2015'];
 
-        /* Random Data Generator (took from nvd3.org) */
-        function generateData(groups, points) {
-          var random = d3.random.normal();
-          var letters = ['A', 'B', 'C'];
-          var data = letters.map(function(text){
-            return {
-             key: 'Events',
-             event: text,
-             values: [],
-             color: 'orange',
-             size: 100,
-            }
-          })
-          for (let i = 0; i < groups; i++) {
+          var eventProps = {shape:'square', color: 'orange'};
+          var valuationProps = {shape:'circle', color: 'green'};
 
-            for (let j = 0; j < points; j++) {
-              data[i].values.push({
-                x: random(),
-                y: 0,
-                sym : "&#xf083",
-                shape: 'square',
-              });
-            }
-          }
+          var valuationEvents = [
+            {id: 1,
+             date: '2013',
+             event: {title: 'something', body: 'super!'},
+             valuation: {price: 99, securityType: 4}
+            },
+            {id: 2,
+             date: '2014',
+             event: {title: 'something',body: 'super!'},
+             valuation: {price: 120,securityType: 4}
+            },
+            {id: 1,
+            date: '2015',
+            event: { title: 'something',body: 'super!'},
+            valuation: {price: 45, securityType: 4}
+             },
+            {id: 1,
+              date: '2016',
+              event: {title: 'something',body: 'super!'},
+              valuation: {price: 67, securityType: 4}
+             }
+          ];
 
+          var valuationGraphData = valuationEvents.map(function(event){
+          var _event = {id: event.id, x: event.date, y: event.valuation.price, valuation: event.valuation, shape: valuationProps.shape};
+          return _event;
+          });
+
+          var eventsGraphData = valuationEvents.map(function(event){
+                var _event = {id: event.id, x: event.date, y: '0', event: event.event, shape: eventProps.shape  };
+            return _event;
+          });
+
+          var data = [{ key: 'Events', values: [], color: 'orange'},
+          { key: 'Price per share', values: [], color: 'green'}]; //{ key: 'Events', values: [], color: 'orange'};
+            data[0].values = eventsGraphData;
+            data[0].values.shift({id: 0, x: 0, y: '0', event: 0, shape: ''  });
+            data[1].values = valuationGraphData;
           return data;
         }
 
-  });
+});
